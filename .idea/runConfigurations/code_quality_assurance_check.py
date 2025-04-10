1c3f495c8eabf1e2ba2464781a19da3c21983814
zacker <zachariepare@gmail.com>
2025-04-09 23:47:25 -0400

    Migrate to UV

--- .idea/runConfigurations/code_quality_assurance_check.py
+++ .idea/runConfigurations/code_quality_assurance_check.py
@@ -1,17 +1,17 @@
 import os
 
-print('Running isort...')
-os.system("poetry run isort sbq_python_template tests")
-print('---------------\n\n')
+print("Running isort...")
+os.system("uv run isort sbq_python_template tests")
+print("---------------\n\n")
 
-print('Running pytest...')
-os.system("poetry run pytest sbq_python_template tests")
-print('---------------\n\n')
+print("Running pytest...")
+os.system("uv run pytest sbq_python_template tests")
+print("---------------\n\n")
 
-print('Running mypy...')
-os.system("poetry run mypy sbq_python_template tests")
-print('---------------\n\n')
+print("Running mypy...")
+os.system("uv run mypy sbq_python_template tests")
+print("---------------\n\n")
 
-print('Running pylint...')
-os.system("poetry run pylint sbq_python_template tests")
-print('---------------\n\n')
+print("Running pylint...")
+os.system("uv run pylint sbq_python_template tests")
+print("---------------\n\n")
